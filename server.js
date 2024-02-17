682ed7541037d76248e1dc9842585105824c9727
Vũ Nguyễn <42742971+vunguyen22271@users.noreply.github.com>
2024-02-17 22:10:34 +0700

    feat(workflows): Add node.js CI pipeline (#1)
    Co-authored-by: vuguyen22271 <vunguyen22271@gmail.com>

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
-  }
-}
+// async function main() {
+//   while(true) {
+//     console.log('Containers rule!');
+//     await sleep(5000);
+//   }
+// }
 
-main();
+// main();
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello!");
+});
+
